02a42c4ee8ba4fd2026c81b23e5460051ac12e38
Thr0in <xthroin@gmail.com>
2024-11-08 01:55:05 +0100

    Added new gui_constants
    Added, TIME_FORMAT, BOLD and DEBUG

--- gui_constants.py
+++ gui_constants.py
@@ -2,7 +2,13 @@
 USE_DATABASE = False
 DAILY_WORKING_HOURS = 8
 NO_TIME_DATA = "--:--"
+TIME_FORMAT = '%H:%M'
+
+# Fonts
+BOLD = ('TkDefaultFont', 9, 'bold')
 
 # Colors
 HIGHLIGHT_COLOR = 'azure3'
 DISABLED_COLOR = 'lightgray'
+
+DEBUG = False
